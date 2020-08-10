389e097eb635aeb0910177a477fcb1fe52d655dd
modo7a <mohamed.mamdouh43@gmail.com>
2020-08-10 18:12:10 +0100

    momo corp website uploaded

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// global $
+
+$(document).ready(function() {
+    'use strict';
+    $('.info-list li').click(function() {
+        $(this).addClass('selected').siblings('li').removeClass('selected');
+        $('.info-content div').hide();
+        $('.' + $(this).data('class')).fadeIn();
+
+    });
+
+
+});
